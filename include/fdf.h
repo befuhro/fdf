7be36550e2cb35da3f3cd2132bd93e4768c3da7a
benjamin <fuhrobenjamin@gmail.com>
2017-12-08 18:08:26 +0100

    mise au propre

--- include/fdf.h
+++ include/fdf.h
@@ -7,4 +7,16 @@ typedef struct	s_coord
 	int x;
 	int y;
 	int z;
-}				t_coord;
+}		t_coord;
+
+typedef struct	s_mlx
+{
+	void	*init;
+	void	*win;
+}		t_mlx;
+
+typedef  struct	s_whole
+{
+	int latitude;
+	t_coord **matrix;
+}		t_whole;
