d2e2783d2f1ec2a16a54f2ff2ac4642c7c319d02
Benjamin Fuhro <befuhro@z1r4p5.le-101.fr>
2018-01-16 19:56:43 +0100

    commit avant reboot

--- srcs/rely_point.c
+++ srcs/rely_point.c
@@ -6,78 +6,84 @@
 /*   By: befuhro <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/01/16 16:31:23 by befuhro      #+#   ##    ##    #+#       */
-/*   Updated: 2018/01/16 16:56:42 by befuhro     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/01/16 19:55:30 by befuhro     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	print_color(t_whole *whole, t_coord point)
+void	print_color(t_whole *whole, t_coord *point)
 {
-	mlx_pixel_put(whole->init, whole->win, point.x, point.y, 0x00FEFFFF);
+	mlx_pixel_put(whole->init, whole->win, point->x, point->y, 0x00FEFFFF);
 }
+
+void	manage_z(t_coord begin, t_coord end, t_coord *line)
+{
+	
+
+
+
+
+}
+
+
+
+
+
 
 void	trace_higher(t_coord begin, t_coord end, t_whole *whole)
 {
-	float ratio;
-	float y1;
+	float	ratio;
+	t_coord	line;
 
 	ratio = (begin.y - end.y) / (end.x - begin.x);
-	y1 = begin.y;
-	while (begin.x != end.x && begin.y != end.y)
+	line = begin;
+	while (line.x != end.x && line.y != end.y)
 	{
 		if (ratio < 1)
 		{
-			begin.x++;
-			begin.y -= ratio;
+			line.x++;
+			line.y -= ratio;
 		}
 		else
 		{
-			begin.y--;
-			if (y1 - begin.y >= ratio)
+			line.y--;
+			if (begin.y - line.y >= ratio)
 			{
-				begin.x++;
+				line.x++;
 				ratio += (begin.y - end.y) / (end.x - begin.x);
 			}
 		}
-		print_color(whole, begin);
+		print_color(whole, &line);
 	}
 }
 
 void	trace_lower(t_coord begin, t_coord end, t_whole *whole)
 {
-	float ratio;
-	float y1;
+	float	ratio;
+	t_coord	line;
 
 	ratio = (end.y - begin.y) / (end.x - begin.x);
-	y1 = begin.y;
-	while (begin.x != end.x && begin.y != end.y)
+	line = begin;
+	while (line.x != end.x && line.y != end.y)
 	{
 		if (ratio < 1)
 		{
-			begin.x++;
-			begin.y += ratio;
+			line.x++;
+			line.y += ratio;
 		}
 		else
 		{
-			begin.y++;
-			if (begin.y - y1 >= ratio)
+			line.y++;
+			if (line.y - begin.y >= ratio)
 			{
-				begin.x++;
+				line.x++;
 				ratio += (end.y - begin.y) / (end.x - begin.x);
 			}
 		}
-		print_color(whole, begin);
+		print_color(whole, &line);
 	}
-}
-
-void	trace_line(t_coord begin, t_coord end, t_whole *whole)
-{
-	if (end.y - begin.y < 0)
-		trace_higher(begin, end, whole);
-	else
-		trace_lower(begin, end, whole);
 }
 
 void	rely_point(t_coord **matrix, int width, int height, t_whole *whole)
@@ -85,19 +91,26 @@ void	rely_point(t_coord **matrix, int width, int height, t_whole *whole)
 	int i;
 	int j;
 
-	i = 0;
-	j = 0;
-	while (i < height)
+	i = -1;
+	while (++i < height)
 	{
-		while (j < width)
+		j = -1;
+		while (++j < width)
 		{
 			if (j < width - 1)
-				trace_line(matrix[i][j], matrix[i][j + 1], whole);
+			{
+				if (matrix[i][j + 1].y - matrix[i][j].y < 0)
+					trace_higher(matrix[i][j], matrix[i][j + 1], whole);
+				else
+					trace_lower(matrix[i][j], matrix[i][j + 1], whole);
+			}
 			if (i < height - 1)
-				trace_line(matrix[i][j], matrix[i + 1][j], whole);
-			j++;
+			{
+				if (matrix[i + 1][j].y - matrix[i][j].y < 0)
+					trace_higher(matrix[i][j], matrix[i + 1][j], whole);
+				else
+					trace_lower(matrix[i][j], matrix[i + 1][j], whole);
+			}
 		}
-		i++;
-		j = 0;
 	}
 }
